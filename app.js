f5c162db14745ac0cb1db5b95cf583e8c6d23568
Shahad Tuglak <56908101+Shahadtuglak@users.noreply.github.com>
2021-03-08 17:21:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// Return Input Text and Attach to the Button also (Using btn Function)
+function btn(){
+    const inputValue = document.getElementById("inputText").value;
+    document.querySelector("#returnValue").innerHTML = `You  Wrote ${inputText.value} &` 
+    document.querySelector("#counter").innerHTML = "You Have Written " + inputValue.length + " Digits";
+
+}
+
+
+// function printValue(){
+    // document.querySelector("#returnValue").innerHTML = `You Have Written {"inputText"}` ;
+// }
+
+
+// const Value = () =>
+//  {
+//     document.getElementById("returnValue").innerHTML = "You Have Written " + newVar;
+//  } 
+
+
+
+
+
+
+
+//Test Case
+
+// function btn(){
+//     const inputValue = document.getElementById("inputText").value;
+//     console.log(inputValue);
+// }
